9f54da29c1acf380650dfb22fd534be063fd02dc
dlsso <supertonesfan@hotmail.com>
2014-06-23 14:01:39 -0600

    Actually set up phone validator

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var number = prompt("Please enter your phone number with dashes.\nWe will never spam you or sell your phone numer.")
+var phoneNumber = prompt("Please enter your phone number with dashes.\nWe will never spam you or sell your phone numer.")
+
+	if(phoneNumber.charAt(3) === '-' && phoneNumber.charAt(7)) {
+		var pvalid = "valid."
+	}
+	else{
+		var pvalid = "not valid."
+	};
+
+alert("Phone number is " + pvalid);
